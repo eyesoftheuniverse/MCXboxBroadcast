e525920fca281859bcf80c64254b37a8abe42328
eyesoftheuniverse <izeaack@gmail.com>
2024-08-22 15:33:31 -0400

    Update build.gradle.kts

--- bootstrap/geyser/build.gradle.kts
+++ bootstrap/geyser/build.gradle.kts
@@ -7,18 +7,6 @@ relocate("com.fasterxml.jackson")
 relocate("com.google.gson")
 relocate("net.raphimc.minecraftauth")
 
-repositories {
-    mavenCentral()
-maven {
-		name 'Terraformers'
-		url 'https://repo.maven.apache.org/maven2/pe/pi/sctp4j/1.0.7-SNAPSHOT/maven-metadata.xml'
-	}
-maven {
-		name 'f'
-		url 'https://repo.maven.apache.org/maven2/pe/pi/sctp4j/1.0.7-SNAPSHOT/sctp4j-1.0.7-SNAPSHOT.pom'
-	}
-}
-
 dependencies {
     api(project(":core"))
     api(libs.bundles.jackson)
